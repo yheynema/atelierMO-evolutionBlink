95bdf8b0c9c594c87a89fa1a7f51d110f323610a
Yanick H <yheynema@gmail.com>
2022-09-05 22:44:19 -0400

    3e-version
    Version dont on affiche au port série la valeur lue au potentiomètre, tout simplement. Il n'y a plus de clignotement...

--- atelierMO-evolutionBlink.ino
+++ atelierMO-evolutionBlink.ino
@@ -1,43 +1,25 @@
 /*
-  Blink
+  Version modifiée pour afficher au port série la valeur lue au potentiomètre.
+*/
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+/*
+Afficher valeurs au port série
 */
 
 int potValue = 0;
+const int delais = 250;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(4, OUTPUT);
-  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("Initialisation terminée - je suis prêt!");
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
   potValue = analogRead(15);
-  digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(2, LOW);
-  delay(potValue);                       // wait for a second
-  digitalWrite(4, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(2, HIGH);
-  delay(potValue);                       // wait for a second
+  Serial.println(potValue);
+  delay(delais);                    // wait for a second
 }
